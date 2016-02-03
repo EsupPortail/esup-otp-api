728986cd57ab01c807c7e6b885bce9e4c3bf73aa
Hakall <abouskine@gmail.com>
2016-02-03 12:10:10 +0100

    add params checker

---
+++ services/validator.js
@@ -0,0 +1,18 @@
+var restify = require('restify');
+
+function check_post_parameters(req) {
+    if (req.params.firstname == undefined || req.params.lastname == undefined || req.params.password == undefined) {
+        return false
+    } else {
+    	console.log('ok ?');
+        return true;
+    }
+}
+
+exports.create_user = function(req, res, next) {
+    if (check_post_parameters(req)) {
+        return next();
+    } else {
+        return next(new restify.InvalidArgumentError());
+    }
+}
