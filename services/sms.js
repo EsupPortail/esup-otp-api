6086723ba1dd02f0507497384f7ad631a51734da
Hakall <abouskine@gmail.com>
2016-02-25 12:00:13 +0100

    working with sms api

--- services/sms.js
+++ services/sms.js
@@ -1,4 +1,13 @@
+var httpRequest = require('http_request'); 
+
 exports.send_code = function(tel, message, res) {
-    console.log("Message sent to "+tel+", with the message: "+message);
-    res.send("Message sent to "+tel+", with the message: "+message);
+    console.log("Message sent to " + tel + ", with the message: " + message);
+    httpRequest.get('https://sms.univ-paris1.fr/esup-smsuapi/?action=SendSms&phoneNumber=0652328590&message='+message, {
+        auth: {
+            username: 'esup-otp-api',
+            password: 'chat auto air'
+        }
+    }).then(function(response) {
+        res.send(response.getBody());
+    });
 }
