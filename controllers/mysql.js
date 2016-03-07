9afc78bcd4762b4994ee75c82346a35c26a92cc5
Hakall <abouskine@gmail.com>
2016-03-07 10:06:03 +0100

    test mysql

---
+++ controllers/mysql.js
@@ -0,0 +1,19 @@
+var properties = require(process.cwd() + '/properties/properties');
+var mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+ 
+  console.log('The solution is: ', rows[0].solution);
+});
+ 
+connection.end();
