de1a1d9ff49a3585af4560b10181c6d61f7be001
Hakall <abouskine@gmail.com>
2016-03-15 09:57:13 +0100

    config login methods

--- controllers/api.js
+++ controllers/api.js
@@ -5,16 +5,18 @@ exports.get_available_methods = function(req, res, next) {
 
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    
+
     var response = {
-        "code" : "Error",
-        "message" : "No method found"
+        "code": "Error",
+        "message": "No method found"
     };
     response.methods = [];
     for (method in properties.esup.methods) {
-        response.methods.push(method);
-        response.code = "Ok";
-        response.message = "Method(s) found";
+        if (properties.esup.methods[method].login_view) {
+            response.methods.push(method);
+            response.code = "Ok";
+            response.message = "Method(s) found";
+        }
     }
     res.send(response);
 }
