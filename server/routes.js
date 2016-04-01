1d70994e98f582dcb82716e6107baf3cd7a7a645
Hakall <abouskine@gmail.com>
2016-04-01 09:35:07 +0200

    routes secured

--- server/routes.js
+++ server/routes.js
@@ -13,21 +13,21 @@ exports.initialize = function(server, userDb_controller, apiDB_controller, callb
     server.get("/send_code/:method/:transport/:uid/:hash", validator.send_code, apiDB_controller.send_code);
     
     //salt_level2
-    server.get("/generate/:method/:uid", validator.generate, apiDB_controller.generate);
-    server.get("/secret/google_authenticator/:uid", validator.get_google_authenticator_secret, apiDB_controller.get_google_authenticator_secret);
-    server.put("/deactivate/:method/:uid", validator.toggle_method, apiDB_controller.deactivate_method);
-    server.put("/activate/:method/:uid", validator.toggle_method, apiDB_controller.activate_method);
-    server.put("/transport/:transport/:uid/:new_transport", validator.update_transport, userDb_controller.update_transport);
-    server.post("/verify_code/:uid/:otp", validator.verify_code, apiDB_controller.verify_code);
+    server.get("/generate/:method/:uid/:hash", validator.generate, apiDB_controller.generate);
+    server.get("/secret/google_authenticator/:uid/:hash", validator.get_google_authenticator_secret, apiDB_controller.get_google_authenticator_secret);
+    server.put("/deactivate/:method/:uid/:hash", validator.toggle_method, apiDB_controller.deactivate_method);
+    server.put("/activate/:method/:uid/:hash", validator.toggle_method, apiDB_controller.activate_method);
+    server.put("/transport/:transport/:uid/:new_transport/:hash", validator.update_transport, userDb_controller.update_transport);
+    server.post("/verify_code/:uid/:otp/:hash", validator.verify_code, apiDB_controller.verify_code);
     
     // routes DEV/ADMIN uniquement
     //salt_level2
-    server.get("admin/user/:uid", validator.get_user, apiDB_controller.get_user);
-    server.get("admin/users/drop", apiDB_controller.drop);
-    server.put("admin/deactivate/:method", validator.toggle_method_admin, utils.deactivate_method_admin);
-    server.put("admin/activate/:method", validator.toggle_method_admin, utils.activate_method_admin);
-    server.put("admin/deactivate/:method/:transport", validator.toggle_method_transport, utils.deactivate_method_transport);
-    server.put("admin/activate/:method/:transport", validator.toggle_method_transport, utils.activate_method_transport);
+    server.get("admin/user/:uid/:hash", validator.get_user, apiDB_controller.get_user);
+    server.get("admin/users/drop/:hash", apiDB_controller.drop);
+    server.put("admin/deactivate/:method/:hash", validator.toggle_method_admin, utils.deactivate_method_admin);
+    server.put("admin/activate/:method/:hash", validator.toggle_method_admin, utils.activate_method_admin);
+    server.put("admin/deactivate/:method/:transport/:hash", validator.toggle_method_transport, utils.deactivate_method_transport);
+    server.put("admin/activate/:method/:transport/:hash", validator.toggle_method_transport, utils.activate_method_transport);
 
     if (typeof(callback) === "function") callback(server);
 }
--- services/validator.js
+++ services/validator.js
@@ -3,23 +3,28 @@ var properties = require(process.cwd() + '/properties/properties');
 var TwinBcrypt = require('twin-bcrypt');
 
 var required = {
-    create_user: ['uid'],
-    get_user: ['uid'],
-    set_otp: ['uid', 'otp'],
+    create_user: ['uid', 'hash'],
+    get_user: ['uid', 'hash'],
+    set_otp: ['uid', 'otp', 'hash'],
     get_available_transports: ['uid', 'hash'],
-    verify_code: ['uid', 'otp'],
+    verify_code: ['uid', 'otp', 'hash'],
     send_code: ['uid', 'method', 'transport', 'hash'],
-    generate: ['uid', 'method'],
-    get_google_authenticator_secret: ['uid'],
-    toggle_method: ['uid', 'method'],
-    update_transport: ['uid', 'transport', 'new_transport'],
-    toggle_method_transport: ['transport', 'method'],
+    generate: ['uid', 'method', 'hash'],
+    get_google_authenticator_secret: ['uid', 'hash'],
+    toggle_method: ['uid', 'method', 'hash'],
+    update_transport: ['uid', 'transport', 'new_transport', 'hash'],
+    toggle_method_transport: ['transport', 'method', 'hash'],
     get_activate_methods: ['uid', 'hash'],
-    toggle_method_admin: ['method'],
+    toggle_method_admin: ['method', 'hash'],
 }
 
-function compare_salt_level1(req, res, next) {
+function compare_salt(req, res, next) {
     if (TwinBcrypt.compareSync(req.params.uid + properties.esup.salt, req.params.hash)) return next();
+    else return next(new restify.ForbiddenError());
+}
+
+function compare_secret_salt(req, res, next) {
+    if (TwinBcrypt.compareSync(properties.esup.secret_salt, req.params.hash)) return next();
     else return next(new restify.ForbiddenError());
 }
 
@@ -66,7 +71,7 @@ exports.set_otp = function(req, res, next) {
 
 exports.get_available_transports = function(req, res, next) {
     if (check_parameters(req, required.get_available_transports)) {
-        compare_salt_level1(req, res, next);
+        compare_salt(req, res, next);
     } else {
         return next(new restify.InvalidArgumentError());
     }
@@ -74,7 +79,7 @@ exports.get_available_transports = function(req, res, next) {
 
 exports.get_activate_methods = function(req, res, next) {
     if (check_parameters(req, required.get_activate_methods)) {
-        compare_salt_level1(req, res, next);
+        compare_salt(req, res, next);
     } else {
         return next(new restify.InvalidArgumentError());
     }
@@ -114,7 +119,7 @@ exports.toggle_method_admin = function(req, res, next) {
 
 exports.send_code = function(req, res, next) {
     if (check_parameters(req, required.send_code)) {
-        return next();
+        compare_salt(req, res, next);
     } else {
         return next(new restify.InvalidArgumentError());
     }
