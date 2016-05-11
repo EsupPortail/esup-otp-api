c614b582b11ba30df07c90e4c9b1c6ad7cb3523a
Hakall <abouskine@gmail.com>
2016-05-11 11:58:23 +0200

    test db connections

--- test/test.js
+++ test/test.js
@@ -1,11 +1,17 @@
 var assert = require('chai').assert;
 var os = require('os');
 var request = require('request');
+var mongoose = require('mongoose');
 
 var properties = require(__dirname + '/../properties/properties');
 
-describe('Esup otp api', function(){
-    it('should throw error if server is not running', function(done) {
+
+var mongo_connection;
+var userDb_connection;
+
+
+describe('Esup otp api', function() {
+    it('Server is running', function(done) {
         var port = process.env.PORT || 3000;
         var url = 'http://' + os.hostname() + ':' + port;
         request({ url: url }, function(error, response, body) {
@@ -13,4 +19,17 @@ describe('Esup otp api', function(){
             done();
         });
     });
+
+    it('Mongodb is reachable', function(done) {
+        mongo_connection = mongoose.createConnection('mongodb://' + properties.esup.mongodb.address + '/' + properties.esup.mongodb.db, function(error) {
+            if (error) throw error;
+            done();
+        });
+    });
+
+    it('UserDB is reachable', function(done) {
+        userDb_controller = require(__dirname+ '/../databases/user/' + properties.esup.userDb);
+        userDb_controller.initialize(done);
+    });
+
 });
