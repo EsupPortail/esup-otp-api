ccaa9fffd01a9cae97f8919b883e03ee57717122
Hakall <abouskine@gmail.com>
2016-05-11 16:48:20 +0200

    test get user infos with wrong hash or with unknown user (and auto_create_user)

--- test/user.api.js
+++ test/user.api.js
@@ -3,6 +3,7 @@ var os = require('os');
 var request = require('request');
 var utils = require(__dirname + '/../services/utils');
 var properties = require(__dirname + '/../properties/properties');
+var default_properties = properties;
 
 var mongo_connection;
 var port = process.env.PORT || 3000;
@@ -20,7 +21,7 @@ exports.run = function(mongoconn) {
 }
 
 function test() {
-    before(function() {
+    beforeEach(function() {
         var user = new UserModel();
         user.uid = "test_user";
         user.save(function(raw) {})
@@ -35,8 +36,31 @@ function test() {
         });
     })
 
-    after(function() {
+    it('get test_user with wrong hash', function(done) {
+    	var url = server_url + '/user/test_user/turlututuchapeaupointu'
+        request({ url: url }, function(error, response, body) {
+            if (error) throw error;
+            assert(JSON.parse(body).code == 'ForbiddenError');
+            done();
+        });
+    })
+
+    it('get unknown user with auto_create', function(done) {
+    	var url = server_url + '/user/unknown_user/'+ utils.get_hash('unknown_user')[1]
+        request({ url: url }, function(error, response, body) {
+            if (error) throw error;
+            assert(JSON.parse(body).code == 'Ok');
+            done();
+        });
+    })
+
+    afterEach(function() {
+    	properties = default_properties;
+
         UserModel.remove({ uid: 'test_user' }, function(err, data) {
+            if (err) throw err;
+        });
+        UserModel.remove({ uid: 'unknown_user' }, function(err, data) {
             if (err) throw err;
         });
     })
