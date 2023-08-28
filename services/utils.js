23fcaf136b36711219b1f6ffe3854850b99945ee
Florian Nari <Florian.Nari@univ-paris1.fr>
2023-08-28 12:00:36 +0200

    allow international phone number

--- services/utils.js
+++ services/utils.js
@@ -51,7 +51,7 @@ exports.generate_digit_code = function(code_length) {
 
 exports.check_transport_validity= function(transport, value){
     var reg;
-    if (transport == 'sms') reg = new RegExp("^0[6-7]([-. ]?[0-9]{2}){4}$");
+    if (transport == 'sms') reg = new RegExp("^((0[67](([.]|[-]|[ ])?[0-9]){8})|((00|[+])(([.]|[-]|[ ])?[0-9]){7,15}))$");
     else reg = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
     return reg.test(value);
 }
