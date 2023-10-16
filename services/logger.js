0b7a1c277e65c23f3e80ca7cb71c4502848f2bb3
mbdeme <mbdeme@univ-paris1.fr>
2023-10-16 11:08:04 +0200

    chore: update Winston to version 3.x, which includes notable changes to the logger API and configuration

--- services/logger.js
+++ services/logger.js
@@ -1,39 +1,34 @@
-var winston = require('winston');
-var properties = require(__dirname + '/../properties/properties');
-var logs_config = require(__dirname + '/../logs/logs.json');
-var utils = require('./utils');
+const winston = require('winston');
+const properties = require(__dirname + '/../properties/properties');
+const logs_config = require(__dirname + '/../logs/logs.json');
+const utils = require('./utils');
 
-var filename = __dirname+'/../logs/esup-otp-api-info.log';
+let filename = __dirname + '/../logs/esup-otp-api-info.log';
 if(logs_config.path && logs_config.filename)filename = logs_config.path + logs_config.filename;
 
-var logger = new (winston.Logger)({
-    transports: [
-        new (winston.transports.Console)({
-            timestamp: function() {
-                return new Date(Date.now());
-            },
-            level: logs_config.level || "info",
-            formatter: function(options) {
-                // Return string will be passed to logger.
-                return options.timestamp() +' '+ options.level.toUpperCase() +' '+ (undefined !== options.message ? options.message : '') +
-                    (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '' );
-            }
-        }),
-        new (winston.transports.File)({
-            timestamp: function() {
-                return ''+new Date(Date.now());
-            },
-            name: 'log-file',
-            level: logs_config.level || "info",
-            filename: filename,
-            json: false,
-            formatter: function(options) {
-                // Return string will be passed to logger.
-                return options.timestamp() +' '+ options.level.toUpperCase() +' '+ (undefined !== options.message ? options.message : '') +
-                    (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '' );
-            }
-        })
-    ]
+const logger = winston.createLogger({
+  level: logs_config.level || 'info',
+  format: winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.printf(({ timestamp, level, message, meta }) => {
+      let logMessage = `${timestamp} ${level.toUpperCase()} ${message || ''}`;
+      if (meta && Object.keys(meta).length > 0) {
+        logMessage += `\n\t${JSON.stringify(meta)}`;
+      }
+      return logMessage;
+    })
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      level: logs_config.level || 'info',
+      filename: filename,
+      format: winston.format.combine(
+        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        winston.format.uncolorize()
+      ),
+    }),
+  ],
 });
 
 exports.getInstance= function(){
