f1d84bb3d38be5691d8bfbdf1888cabc0c109b1f
Guillaume Rousse <guillaume.rousse@renater.fr>
2025-05-27 11:22:07 +0200

    rely on configuration instead of client behaviour
    Which secret  to use(either global or tenant-specific) should
    dependend from API configuration, not from request format.

--- services/validator.js
+++ services/validator.js
@@ -8,16 +8,21 @@ const logger = getInstance();
 
 export async function check_hash(req, res) {
     let users_secret;
-    const tenant = req.header('x-tenant');
-    if (tenant) {
-        // use tenant-specific users secret
+    const tenants = properties.getEsupProperty('tenants');
+    if (tenants && tenants.length) {
+        // multi-tenants support is active: require tenant header, and use
+        // tenant-specific users secret
+        const tenant = req.header('x-tenant');
+        if (!tenant) {
+            throw new errors.BadRequestError();
+        }
         const dbTenant = await apiDb.find_tenant_by_name(tenant);
         if (!dbTenant) {
             throw new errors.BadRequestError();
         }
         users_secret = dbTenant.users_secret;
     } else {
-        // use global users secret
+        // multi-tenants support is not active: use global users secret
         users_secret = properties.getEsupProperty('users_secret');
     }
 
@@ -33,16 +38,21 @@ export function check_hash_socket(uid, hash, users_secret) {
 
 export async function check_api_password(req, res) {
     let api_password;
-    const tenant = req.header('x-tenant');
-    if (tenant) {
-        // use tenant-specific API password
+    const tenants = properties.getEsupProperty('tenants');
+    if (tenants && tenants.length) {
+        // multi-tenants support is active: require tenant header, and use
+        // tenant-specific API password
+        const tenant = req.header('x-tenant');
+        if (!tenant) {
+            throw new errors.BadRequestError();
+        }
         const dbTenant = await apiDb.find_tenant_by_name(tenant);
         if (!dbTenant) {
             throw new errors.BadRequestError();
         }
         api_password = dbTenant.api_password;
     } else {
-        // use global API password
+        // multi-tenants support is not active: use global API password
         api_password = properties.getEsupProperty('api_password');
     }
 
