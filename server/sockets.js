f481370ae235264174ee898f5e08647361785e24
Florian Nari <florian.nari@univ-paris1.fr>
2025-11-17 17:32:40 +0100

    better socket.io refactor

--- server/sockets.js
+++ server/sockets.js
@@ -8,21 +8,10 @@ let io;
 import * as properties from '../properties/properties.js';
 import * as validator from '../services/validator.js';
 import { logger } from '../services/logger.js';
-/**
- * @import { Room } from "socket.io-adapter"
- *
- * @type { Object.<String:Set<Room>>}
- */
-const managerSockets = {};
 
 import * as socket_io from 'socket.io';
 import restifyErrors from 'restify-errors';
 import * as multiTenantUtils from '../services/multiTenantUtils.js';
-
-/**
- * @type { Object.<String:Room>}
- */
-const users = {};
 
 export function attach(server){
     const casVhost = properties.getEsupProperty("casVhost")
@@ -62,25 +51,15 @@ async function connectManager(socket, query) {
     const tenant = await multiTenantUtils.getCurrentTenantPropertiesInternal(null, socket.handshake.headers[multiTenantUtils.TENANT_HEADER]);
     await validator.check_protected_access_internal(tenant, query.secret, socket.handshake.headers);
 
-    const sockets = getManagerSockets(tenant);
-
-    sockets.add(socket.id);
-
-    socket.on('disconnect', function() {
-        sockets.delete(socket.id)
-    });
+    socket.join(managerRoom(tenant));
 }
 
 async function connectCasUser(socket, query) {
     await validator.check_hash({ params: query });
 
-    users[query.uid] = socket.id;
-
-    socket.on('disconnect', function() {
-        if (users[query.uid] === socket.id) {
-            delete users[query.uid];
-        }
-    });
+    const room = userRoom(query.uid)
+    io.socketsLeave(room); // clear room https://socket.io/fr/docs/v4/server-instance/#socketsleave
+    socket.join(room);
 }
 
 export async function close() {
@@ -89,17 +68,17 @@ export async function close() {
 
 export async function emitManager(req, eventName, data) {
     const tenant = await multiTenantUtils.getCurrentTenantProperties(req);
-    io.to(Array.from(getManagerSockets(tenant))).emit(eventName, data);
-}
-
-/** @returns {Set<Room>} */
-function getManagerSockets(tenant) {
-    return managerSockets[tenant.id] ||= new Set();
+    io.to(managerRoom(tenant)).emit(eventName, data);
 }
 
 export function emitUserAuth(uid, otp) {
-    if (users[uid]) {
-        io.to(users[uid])
-            .emit('userAuth', { code: "Ok", otp, });
-    }
+    io.to(userRoom(uid)).emit('userAuth', { code: "Ok", otp, });
+}
+
+function managerRoom(tenant) {
+    return "tenant-" + tenant.id;
+}
+
+function userRoom(uid) {
+    return "user-" + uid;
 }
