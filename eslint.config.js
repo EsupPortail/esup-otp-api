98f4b0f14a1f5353677b8b43521aa1f820604281
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2026-01-13 11:18:59 +0100

    build: disallow modern JS syntax in public/login.js (needed for pre-Chromium MS-Edge used in Microsoft apps (Office 365, Onedrive...))

--- eslint.config.js
+++ eslint.config.js
@@ -57,6 +57,12 @@ export default defineConfig([
             "no-empty": ["error", {
                 "allowEmptyCatch": true,
             }],
+            // we must be backward compatible with pre-Chromium MS-Edge, used in Microsoft apps (Office 365, Onedrive...)
+            // (user-agent : Mozilla/5.0 (Windows NT 10.0; Win64; x64; WebView/3.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.22631)
+            // so disallow modern JS syntax:
+            "no-restricted-syntax": ["error", 
+                "ChainExpression", "ObjectExpression > SpreadElement", "LogicalExpression[operator='??']"
+            ],
         },
     }
 ]);
