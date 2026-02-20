d424b1efdbadd51f822fac8de9bcdce187334b5f
Florian Nari <florian.nari@univ-paris1.fr>
2026-02-20 18:01:01 +0100

    chore(deps-dev): bump eslint from 9.x to 10.x

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,11 @@
 import { defineConfig } from "eslint/config";
 import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
 
 export default defineConfig([
     {   // nodejs
         ignores: ["public/**"],
-        extends: compat.extends("eslint:recommended"),
+        extends: [js.configs.recommended],
         languageOptions: {
             globals: {
                 ...globals.node,
@@ -37,7 +26,7 @@ export default defineConfig([
     },
     {   // browser
         files: ["public/**/*.js"],
-        extends: compat.extends("eslint:recommended"),
+        extends: [js.configs.recommended],
         languageOptions: {
             globals: {
                 ...globals.browser,
@@ -60,7 +49,7 @@ export default defineConfig([
             // we must be backward compatible with pre-Chromium MS-Edge, used in Microsoft apps (Office 365, Onedrive...)
             // (user-agent : Mozilla/5.0 (Windows NT 10.0; Win64; x64; WebView/3.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.22631)
             // so disallow modern JS syntax:
-            "no-restricted-syntax": ["error", 
+            "no-restricted-syntax": ["error",
                 "ChainExpression", "ObjectExpression > SpreadElement", "LogicalExpression[operator='??']"
             ],
         },
